5977754f2caabd3b6e9b1ca9b858d0b3b74cbb67
Anatoly <abrekov@ya.ru>
2019-03-20 15:59:26 +0400

    some minor changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import readlineSync from 'readline-sync';
 
-const userName = () => {
+export const userName = () => {
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
 
-export default userName;
+export const isEven = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log()
+}
