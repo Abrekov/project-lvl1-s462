5ffb29968ad97376a6456ad5c7e7506a61efea3b
Anatoly <abrekov@ya.ru>
2019-03-20 23:55:52 +0400

    add 4 step

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Hexlet project "Brain Games"
 <a href="https://codeclimate.com/github/Abrekov/project-lvl1-s462/test_coverage"><img src="https://api.codeclimate.com/v1/badges/7e979d45960555010423/test_coverage" /></a>
 
 [![Build Status](https://travis-ci.org/Abrekov/project-lvl1-s462.svg?branch=master)](https://travis-ci.org/Abrekov/project-lvl1-s462)
+
+https://asciinema.org/a/R9nXGYjkxYs4VszRHywPH7m25
--- src/index.js
+++ src/index.js
@@ -8,5 +8,29 @@ export const userName = () => {
 
 export const isEven = () => {
   console.log('Welcome to the Brain Games!');
-  console.log()
-}
+  console.log('Answer "yes" if number even otherwise answer "no"');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+
+  let num = 0;
+  let answer = '';
+  let correctAnswer = '';
+
+  for (let i = 0; i < 3; i += 1) {
+    num = Math.floor(Math.random() * Math.floor(100));
+    console.log(`Question: ${num}`);
+    answer = readlineSync.question('Your answer: ');
+    correctAnswer = num % 2 === 0 ? 'yes' : 'no';
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
+      console.log(`Let's try again, ${name}`);
+      break;
+    }
+  }
+
+  if (answer === correctAnswer) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
